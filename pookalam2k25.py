b2e7a127be1093aadb773a335192f5973b6c91d6
Sarayu Suresh <sarayusuresh18@gmail.com>
2025-09-04 20:35:27 +0530

    Add files via upload

---
+++ pookalam2k25.py
@@ -0,0 +1,23 @@
+c1 = circle(x=0,y=0,r=149,fill="green",stroke="none")
+r1=rectangle(w=210,h=210,fill="red",stroke="none") | repeat(20,rotate(50))
+r2=rectangle(w=195,h=195,fill="orange",stroke="none") |rotate(15)| repeat(15,rotate(50))
+r3=rectangle(w=180,h=180,fill="yellow",stroke="none") |repeat(15,rotate(50))
+r4=rectangle(w=165,h=165,fill="brown",stroke="none") | rotate(15)| repeat(15,rotate(50))
+r5=rectangle(w=140,h=140,fill="purple",stroke="none") | repeat(20,rotate(25))
+e1=ellipse(w=210,h=20,fill="yellow",stroke="purple",stroke_width=3)| repeat(50,rotate(10))
+r11=rectangle(w=80,h=80,fill="green",stroke="none")|rotate(15) | repeat(2,rotate(50))
+r12=rectangle(w=70,h=70,fill="red",stroke="none")| repeat(2,rotate(50))
+r13=rectangle(w=60,h=60,fill="orange",stroke="none")|rotate(15)| repeat(2,rotate(50))
+r14=rectangle(w=50,h=50,fill="yellow",stroke="none")|repeat(2,rotate(50))
+r15=rectangle(w=40,h=40,fill="brown",stroke="none")|rotate(15)| repeat(2,rotate(50))
+r21=rectangle(w=30,h=30,fill="green",stroke="none")|rotate(15) | repeat(2,rotate(50))
+r22=rectangle(w=20,h=20,fill="red",stroke="none")| repeat(2,rotate(50))
+r23=rectangle(w=10,h=10,fill="orange",stroke="none")|rotate(15)| repeat(2,rotate(50))
+r24=rectangle(w=5,h=5,fill="yellow",stroke="none")|repeat(2,rotate(50))
+r25=rectangle(w=4,h=4,fill="brown",stroke="none")|rotate(15)| repeat(2,rotate(50))
+show(c1,r1,r2,r3,r4,r5,e1,r11,r12,r13,r14,r15,r21,r22,r23,r24,r25)
+
+
+
+
+
